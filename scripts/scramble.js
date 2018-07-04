640181a41249e514caba5f979eb5f3faedc7cd2f
sinewyk <sinewyk@gmail.com>
2018-07-04 17:57:03 +0200

    code splitting is enough =) for the leak

--- scripts/scramble.js
+++ scripts/scramble.js
@@ -1,13 +1,8 @@
 const fs = require("fs");
 
-const data = [
-  "index_initial",
-  "index_no_scss",
-  "index_no_alias",
-  "index_with_error",
-  "index_require_error",
-  "index_with_chunk"
-].map(x => fs.readFileSync(`./lib/${x}.js`));
+const data = ["index_initial", "index_with_chunk"].map(x =>
+  fs.readFileSync(`./lib/${x}.js`)
+);
 
 let i = 0;
 
@@ -15,13 +10,6 @@ setInterval(function() {
   fs.writeFileSync(
     "./lib/index.js",
     Buffer.concat([data[i % data.length], Buffer.from(`\nconsole.log(${i})`)])
-  );
-  fs.writeFileSync(
-    "./lib/index2.js",
-    Buffer.concat([
-      data[(i + 1) % data.length],
-      Buffer.from(`\nconsole.log(${i})`)
-    ])
   );
   ++i;
 }, 1000);
--- webpack.config.js
+++ webpack.config.js
@@ -14,57 +14,6 @@ module.exports = {
   mode: "development",
 
   entry: {
-    app1: "./lib/index.js",
-    app2: "./lib/index2.js"
-  },
-
-  resolve: { alias: { lib: path.join(__dirname, "lib") } },
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            cacheDirectory: true,
-            presets: ["babel-preset-env"],
-            plugins: ["babel-plugin-transform-runtime"]
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-
-  plugins: [
-    new wp.NoEmitOnErrorsPlugin(),
-    new wp.DefinePlugin({
-      __DEFINED__: true
-    })
-  ],
-
-  node: {
-    Buffer: false,
-    process: false,
-    global: true,
-    __dirname: true,
-    __filename: true
-  },
-
-  devServer: {
-    noInfo: false,
-    quiet: false,
-    watchOptions: {
-      aggregateTimeout: 20
-    },
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    },
-    disableHostCheck: true
+    app1: "./lib/index.js"
   }
 };
