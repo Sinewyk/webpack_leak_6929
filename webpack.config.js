ec3bff8f978e04f755c8b6595e1d34a626f80b62
sinewyk <sinewyk@gmail.com>
2018-07-04 17:35:02 +0200

    parameterize heapdumps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const wp = require("webpack");
 const path = require("path");
 const hp = require("heapdump");
 
+if (process.env.GENERATE_HEAP_DUMP) {
+  setInterval(() => {
+    hp.writeSnapshot(function(err, filename) {
+      console.log("dump written to", filename);
+    });
+  }, parseInt(process.env.HEAPDUMP_INTERVAL, 10));
+}
+
 module.exports = {
   mode: "development",
 
