6e0056aef9d3745b7607cbc39e40596378d7e0f5
sinewyk <sinewyk@gmail.com>
2018-07-04 13:53:36 +0200

    add some plugins step by step

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const wp = require("webpack");
 const path = require("path");
 
 module.exports = {
   mode: "development",
 
   resolve: { alias: { lib: path.join(__dirname, "lib") } },
+
+  plugins: [
+    new wp.NoEmitOnErrorsPlugin(),
+    new wp.DefinePlugin({
+      __DEFINED__: true
+    })
+  ],
 
   entry: "./lib/index.js"
 };
